d9537a49b0c2102283f55bb45c30e5e58a757157
Raviteja474 <pamujula.raviteja@gmail.com>
2022-06-17 21:48:51 +0530

    June 17

---
+++ Batch_4/passport/passport_registration_practice.py
@@ -0,0 +1,136 @@
+# Identifier = variables+constant
+# variable
+# men = input("Enter how many men coming?")
+# women = input("Enter how many women coming?")
+# # constant
+# ENTRY_FEES_WOMEN = 15
+# ENTRY_FEES_MEN = 20
+# nomenclature
+# loop or iteration
+
+# for
+# while
+
+# print("Day 1, Ravi is taking breakfast.")
+# print("Day 2, Ravi is taking breakfast.")
+# print("Day 3, Ravi is taking breakfast.")
+# print("Day 4, Ravi is taking breakfast.")
+# print("Day 5, Ravi is taking breakfast.")
+# print("Day 6, Ravi is taking breakfast.")
+# print("Day 7, Ravi is taking breakfast.")
+#
+# for var in range(8):
+#     print(f"Day {var} Ravi is taking breakfast.")
+
+
+#  KeyboardInterrupt - infinite loop = ctrl+c
+
+# import time
+# aninash_age = 1
+# while aninash_age<18:
+#     print(f"aninash age: {aninash_age}")
+#     # add 1 second
+#     time.sleep(1)
+#     # increment
+#     aninash_age = aninash_age+1
+# print(f"aninash can vote.")
+
+# condition
+# gender = input("What is the gender?")
+# if gender == "male":
+#     print("You are male")
+# elif gender == "female":
+#     print("You are female")
+# else:
+#     print
+
+#input string
+# type conversion  string --> integer
+# integer = 60
+# float = 41.12
+# string_1 = "sowjanya"
+# boolean_flag = True/False
+
+# gender = input("What is the gender?")
+# print(gender)
+# print(type(gender))
+# print("_______________________________________________________________________________")
+# # ValueError: invalid literal for int() with base 10: 'male'
+# age = float(input("What is the age?"))
+# print(age)
+# print(type(age))
+# print("_______________________________________________________________________________")
+# flag = bool(input("What is the flag?"))
+# print(flag)
+# print(type(flag))
+# print("_______________________________________________________________________________")
+# # 19@@@
+# # print(0 or None)
+
+# None = null = null pointer exception
+
+# print(True or False)
+# print(False or False)
+# print(None or False)
+# print(None or True)
+
+# This is the first class.
+# dry run
+# print("Starting")
+# gender = input("What is the gender?")
+# age = int(input("What is the age?"))
+
+
+# # 19@@  spaces or tabs
+# if gender == "male" and age>60:
+#     print("Pension is : 2500")
+# elif gender == "male" and age>45:
+#     print("Pension is : 2250")
+# elif gender == "male":
+#     if age <5:
+#         print("Go and play!!!!!!!!:)")
+#     print("You are youth male")
+# elif gender == "female" and age>45 :
+#     print("Pension is : 3000")
+# elif gender == "female" and age > 60:
+#     print("Pension is : 3250")
+# else:
+#     print("You are others")
+
+# inline = single line comment
+# mulitple line comment
+""""
+Rama is good boy
+I am an indian
+I am from India
+"""
+# ctrl+forward slash
+
+# D:\Raviteja_teaching\Batch_4\passport>python passport_registration_practice.py -d 1
+# Starting
+# What is the gender?male
+# What is the age?45
+# You are youth male
+
+
+print(12+13)
+print(12-13)
+print(12*13)
+print(12%13)
+print(9%7)
+
+# Normal divison
+print(24/13) # 1
+# Floor division is two forward slash
+# 1.83
+print(24//13)
+
+
+
+
+
+
+
+
+
+
