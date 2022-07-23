958d7c135982fa2f2d79e7f49823445ae390ba93
Raviteja474 <pamujula.raviteja@gmail.com>
2022-07-23 15:43:43 +0530

    Jul23

---
+++ Batch_4/oops/oops_example_1.py
@@ -0,0 +1,13 @@
+PI = 3.14
+print(PI)
+
+class Person:
+    # dunder = double underscore, magical method
+    def __init__(self,eyes,legs):
+        self.eyes = eyes
+        self.legs = legs
+
+
+vaibhav = Person(2,2)
+bharath = Person(2,2)
+
